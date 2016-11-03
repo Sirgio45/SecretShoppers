ca7a3ac7ff95894943e583cfa3ead4b45eb2f537
Sirgio45 <15419260@mstc.edu>
2016-11-03 13:55:58 -0500

    Initial commit

---
+++ app/src/main/java/com/ebookfrenzy/secretshoppers/SecretShoppers.java
@@ -0,0 +1,13 @@
+package com.ebookfrenzy.secretshoppers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecretShoppers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secret_shoppers);
+    }
+}
